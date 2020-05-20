d5ce6f7a5bf6f14b97c6cfe589b5de34303eb92e
Badri Tatarashvili <btatarashvili@aldagi.ge>
2020-05-20 16:23:29 +0400

    General Structure

---
+++ GameStore.API/GameStore.API/Helper/Requirements.cs
@@ -0,0 +1,27 @@
+﻿using GameStore.API.Models;
+using Microsoft.AspNetCore.Mvc.ApplicationModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GameStore.API.Helper
+{
+    public class Requirements
+    {
+        [Required]
+        public double Size { get; set; }
+        [Required]
+        public OS OperatingSystem { get; set; }
+        [Required]
+        public int Ram { get; set; }
+        [Required]
+        public string Processor { get; set; }
+        [Required]
+        public string GPU { get; set; }
+        [Required]
+        public Platform Platform { get; set; }
+
+    }
+}
---
+++ GameStore.API/GameStore.API/Models/Game.cs
@@ -0,0 +1,44 @@
+﻿using GameStore.API.Helper;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GameStore.API.Models
+{
+    public enum Platform
+    {
+        PC,
+        XBOX,
+        Playstation,
+        Nintendo
+    }
+    public enum OS 
+    {
+        Windows_XP,
+        Windows_7,
+        Windows_8,
+        Windows_10,
+        Linux,
+        MAC
+    }
+    public class Game
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string InterfaceLanguage { get; set; }
+        [Required]
+        public string Language { get; set; }
+        [Required]
+        public string Genre { get; set; }
+        [Required]
+        public Requirements Requirements { get; set; }
+        public string Description { get; set; }
+        public string Publisher { get; set; }
+        public DateTime ReleaseDate { get; set; }
+    }
+}
